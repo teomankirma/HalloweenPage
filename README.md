9fc9d7a37a33e67e6b5ca56c97c10effa23972c5
teomankirma <83593653+teomankirma@users.noreply.github.com>
2022-11-01 16:11:23 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# HalloweenPage
+ Halloween page.
---
+++ assets/js/script.js
@@ -0,0 +1,21 @@
+"use strict";
+
+// PRELOADER
+const preloader = document.querySelector("[data-preloader]");
+
+window.addEventListener("load", () => {
+  preloader.classList.add("loaded");
+});
+
+// MOBILE NAVBAR TOGGLE
+const [navToggler, navbar] = [
+  document.querySelector("[data-nav-toggler]"),
+  document.querySelector("[data-navbar]"),
+];
+
+const toggleNav = function () {
+  navbar.classList.toggle("active");
+  this.classList.toggle("active");
+};
+
+navToggler.addEventListener("click", toggleNav);
